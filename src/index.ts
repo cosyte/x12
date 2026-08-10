0dd68b2fba899dfd3e099278db63aac19a9c8d71
Noah Schatz <noah.lane.schatz@gmail.com>
2026-08-10 01:15:12 -0400

    Version Packages (#115)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@
  * console.log(VERSION);
  * ```
  */
-export const VERSION: string = "0.0.15";
+export const VERSION: string = "0.0.16";
 
 // Phase 1 - envelope parser surface.
 export { parseX12 } from "./parser/index.js";
